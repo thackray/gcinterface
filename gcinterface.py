831fd1f2999538144a5e98180556c06a4eeaa5aa
thackray <thackray@mit.edu>
2014-04-01 23:32:00 -0400

    adding detail to sketch

--- gcinterface.py
+++ gcinterface.py
@@ -1,28 +1,58 @@
 # rough sketch of functionality
 
+import os, shutil
+from gchem import gchem
+
 class GCFileObj(object):
      def init(self, filename):
           self.filename = filename
 
 class InputGeos(gcfileobj):
-     def write
-     def open_from_template
-     def set_dates
-     def set_emis
-     def set_params
-     
+     def write(self,):
+          # self.filename is set
+          return
+     def open_from_template(self, template_filename):
+          return
+     def set_dates(self, run_start_date, run_end_date):
+          self.run_start_date = run_start_date
+          self.run_end_date = run_end_date
+          return
+     def set_emis(self, emis_filename):
+          self.emission_filename = emis_filename
+          return
+     def set_params(self, params):
+          for param in params:
+               self.param = params[param]
+          return
+
 class RunDir(object):
-     def write_file
-     def copy_file
-     def make_restart
-     
+     def write_file(self, fileobj):
+          fileobj.write()
+          return
+     def copy_file(self, origin, destination):
+          shutil.copy( os.path.join( origin, destination ) )
+          return
+
 class EmisFile(GCFileObj):
-     def scale_emissions
-     def convert_emis_format
+     def scale_emissions(self,):
+          return
+     def convert_emis_format(self,):
+          return
+     def write(self, ):
+          return
      
 class RestartFile(GCFileObj):
-     def init_from_prior
-     def scale_tracers
-     def add_tracer
-     def remove tracer
+     def init_from_prior(self,):
+          return
+     def scale_tracers(self,):
+          return
+     def add_tracer(self,):
+          return
+     def remove_tracer(self,):
+          return
+     def write(self,):
+          # write at self.filename
+          return
+
+
      
