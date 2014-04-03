eab53ecbd47b95a6b33e48aa3c0402658dd704a7
thackray <thackray@mit.edu>
2014-04-02 22:50:25 -0400

    Added input.geos functionality

--- gcinterface.py
+++ gcinterface.py
@@ -2,30 +2,31 @@
 
 
 import os, shutil
-from gchem import gchem
+#from gchem import gchem
 
 class GCFileObj(object):
-     def init(self, filename):
+     def __init__(self, filename):
           self.filename = filename
 
-class InputGeos(gcfileobj):
+class InputGeos(GCFileObj):
      def write(self,):
           # self.filename is set
+          with open(self.filename, 'w') as f:
+               f.write(self.template)
           return
-     def open_from_template(self, template_filename):
-          template = open(template_filename, 'r').read()
-          # string replace @TEMPLATETAGS in the file
+     def load_template(self, template_filename):
+          self.template = open(template_filename, 'r').read()
           return
      def set_dates(self, run_start_date, run_end_date):
-          self.run_start_date = run_start_date
-          self.run_end_date = run_end_date
+          self.template = self.template.replace('@STARTDATE', run_start_date)
+          self.template = self.template.replace('@ENDDATE', run_end_date)
           return
      def set_emis(self, emis_filename):
-          self.emission_filename = emis_filename
+          self.template = self.template.replace('@EMISFILEPATH', emis_filename)
           return
      def set_params(self, params):
           for param in params:
-               self.param = params[param]
+               self.template = self.template.replace('@'+param, params[param])
           return
 
 class RunDir(object):
@@ -57,5 +58,10 @@ class RestartFile(GCFileObj):
           # write at self.filename
           return
 
+if __name__=='__main__':
+     i = InputGeos('testinputfile')
+     i.load_template('input.template')
+     i.set_emis('new/emis/path')
+     i.write()
 
      
